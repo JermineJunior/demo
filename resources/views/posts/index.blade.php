282a23ee4ef3dc29c8eca584e37b6c954857128c
Mohammed Ibrahim <obietrice734@gmail.com>
2020-05-02 13:51:27 +0200

    =permanent ui update

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,21 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-   @forelse ($posts as $post)
-       <div class="card container mx-auto mb-4">
-           <div class="card-title">
-               <a href="{{ $post->path() }}">
-                  {{ $post->title }}
-               </a>
-           </div>
-           <div class="card-body">
-               {{ $post->body }}
-           </div>
-       </div>
-   @empty
-       <span class="text-red-400">
-           Sorry No posts yet.
-       </span>
-   @endforelse
-  
+<div class="container mx-auto">
+    <div class="flex items-center justify-between">
+        <a href="#" class="text-xl text-blue-700 font-semibold">
+            Post
+        </a>
+    
+        <a href="#" class="px-2 py-1 rounded-lg shadow-lg bg-blue-500 text-gray-100">
+            Create Post
+        </a>
+    </div>
+    <div class="flex items-center flex-wrap px-8 mt-16">
+    @forelse ($posts as $post)
+    <div class="bg-white p-8 shadow rounded-lg w-128 justify-between mr-4 mb-12">
+        <div class="flex items-center">
+            <h2 class="text-lg text-blue-700 mr-auto mb-2">
+                <a href="{{ route('posts.show',$post->id) }}">
+                    {{ $post->title }}
+                </a>
+            </h2>
+            <a href="#" class="-mt-24">
+                <img src="{{ asset('images/follower07.jpg') }}" alt="avatar" class="rounded-full w-20 shadow border-2 border-white ">
+            </a>
+        </div>
+        <div class="text-gray-500">
+            {{ $post->body }}
+        </div>
+        <div class="text-left text-blue-500 pl-4 pt-4 flex items-end justify-end">
+            {{ auth()->user()->name }}
+        </div>
+    </div>
+    @empty
+    <span class="text-red-400">
+        Sorry No posts yet.
+    </span>
+    @endforelse
+    </div>
+</div>
 @endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,11 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="card container mx-auto mb-4">
-    <div class="card-title">
-        {{ $post->title }}
+<div class="container mx-auto mb-4">
+    <div class="flex items-center justify-between">
+        <a href="#" class="text-xl text-blue-700 font-semibold">
+            Post
+        </a>
+    
+        <a href="#" class="px-2 py-1 rounded-lg shadow-lg bg-blue-500 text-gray-100">
+            Create Post
+        </a>
     </div>
-    <div class="card-body">
+    <div class="bg-white p-8 shadow rounded-lg w-128 justify-between mr-4 my-12">
+    <div class="flex items-center">
+        <h2 class="text-lg text-blue-700 mr-auto mb-2">
+            <a href="{{ route('posts.show',$post->id) }}">
+                {{ $post->title }}
+            </a>
+        </h2>
+        <a href="#" class="-mt-24">
+            <img src="{{ asset('images/follower07.jpg') }}" alt="avatar" class="rounded-full w-20 shadow border-2 border-white ">
+        </a>
+    </div>
+    <div class="text-gray-500">
         {{ $post->body }}
     </div>
 </div>
