d9d613d129c75acf1835348a66115d461754c03c
Mohammed Ibrahim <obietrice734@gmail.com>
2020-06-05 13:08:04 +0200

    user profile update
    Signed-off-by: Mohammed Ibrahim <obietrice734@gmail.com>

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,24 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="bg-white sm:max-w-full max-w-md rounded overflow-hidden shadow-lg mx-3 my-2">
+<div class="bg-white max-w-full rounded overflow-hidden shadow-lg mx-8 my-2">
     <div class="border-b">
         <!-- First list item -->
         <div class="px-6 py-3 flex items-center justify-between"> 
             <div class="pl-3">
-                <p class="text-sm font-semibold">
+                <h3 class="text-lg font-semibold">
                     {{ $profileUser->name }}
-                </p>
-                <p class="text-xs text-gray-600">
+                </h3>
+                <a href="#" class="text-sm block text-gray-800">
                     {{ $profileUser->email }}
-                </p>
+                </a>
+                <small class="text-sm text-gray-600">
+                    Joined {{ $profileUser->created_at->diffForHumans() }}
+                </small>
             </div>
             <div class="">
                 <img class="shadow w-10 h-10 object-cover rounded-full mx-4" src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=731&q=80" alt="avatar">
             </div>
         </div>
     </div>
-    <div class="border-b">
+    <div class="mb-4">
         <!-- First list item -->
         @if ($profileUser->can('update',$profileUser))
         <div class="px-6 py-4 text-center w-64 md:w-full md:flex md:items-center md:justify-center">
@@ -26,12 +29,12 @@
                 <svg  class="w-6 h-6 pr-1 mr-1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M15.2322 5.23223L18.7677 8.76777M16.7322 3.73223C17.7085 2.75592 19.2914 2.75592 20.2677 3.73223C21.244 4.70854 21.244 6.29146 20.2677 7.26777L6.5 21.0355H3V17.4644L16.7322 3.73223Z" stroke="#4A5568" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
-                Edit Your account.
+                Edit Your profile.
             </a>
         </div>
         @endif
     </div>
-    <div class="content my-4 mx-4">
+    {{-- <div class="content my-4 mx-4">
         <h3 class="text-xl">Posts by {{ $profileUser->name }}:</h3>
         <div class="mt-2 text-gray-800 flex flex-wrap">
             @forelse ($profileUser->posts as $post)
@@ -48,7 +51,7 @@
                 </div>
             </div>
             @endforelse
-        </div>
+        </div> --}}
     </div>
 </div>
 @endsection
